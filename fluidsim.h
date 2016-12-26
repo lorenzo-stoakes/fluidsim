95648804ac52f2b6d8a47a5a85d5c3f1aa4c9c9a
Lorenzo Stoakes <lstoakes@gmail.com>
2016-12-26 20:45:02 +0000

    Add MIN description.

--- fluidsim.h
+++ fluidsim.h
@@ -24,6 +24,7 @@
 /* Iterate through a static array. */
 #define FOR_EACH(__i, __arr) for (__i = 0; __i < ARRAY_SIZE(__arr); __i++)
 
+/* Find minimum of two values. */
 #define MIN(__x, __y) (__x <= __y ? __x : __y)
 
 /* The vulkan queues we care about. */
